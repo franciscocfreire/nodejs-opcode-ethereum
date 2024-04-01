f8940e4e970c83b2737b7306172bb6174cb6faf2
Francisco Freire <franciscocfreire@gmail.com>
2024-04-01 16:48:06 -0300

    Add hash value difficult

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,11 +1,23 @@
 const { GENESIS_DATA } = require('../config');
 
+
+const HASH_LENGTH = 64;
+
+const MAX_HASH_VALUE = parseInt("f".repeat(HASH_LENGTH), 16);
+
 class Block {
     constructor({ blockHeaders }) {
         this.blockHeaders = blockHeaders;
     }
 
-    static mineBlock({ lastBlock }) {
+
+    static calculateBlockTargetHash({ lastBlock }) {
+        return (MAX_HASH_VALUE / lastBlock.blockHeaders.difficulty).toString(16)
+    }
+
+
+    static mineBlock({ lastBlock, beneficiary }) {
+        
 
     }
 
